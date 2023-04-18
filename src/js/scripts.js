300448e1bd55b02354e75fcfc3adb7df7edf9b59
nuriarivasbotello <nuriarivasbotello@gmail.com>
2023-04-18 03:17:17 +0200

    jeje

---
+++ src/js/scripts.js
@@ -0,0 +1,6 @@
+const toggleMenuElement = document.getElementById('toggle-menu');
+const mainNavElement = document.getElementById('main-nav');
+function handleClick() {
+  mainNavElement.classList.toggle('hide');
+}
+toggleMenuElement.addEventListener('click', handleClick);
